a6fcb037e659c9fb5a48372d2b87212824d38d8e
90degs2infty <anon31415@posteo.org>
2023-01-12 15:12:57 +0100

    Implement is_generated

--- cmake/AbsPath.cmake
+++ cmake/AbsPath.cmake
@@ -146,3 +146,27 @@ function(absolutify_source var source target)
 
     set(${var} "${_source_abs}" PARENT_SCOPE)
 endfunction()
+
+function(is_generated var source target)
+    get_target_property(_target_source_dir ${target} SOURCE_DIR)
+    get_target_property(_target_binary_dir ${target} BINARY_DIR)
+
+    set(_generated)
+
+    cmake_path(ABSOLUTE_PATH source BASE_DIRECTORY "${_target_binary_dir}" NORMALIZE OUTPUT_VARIABLE _generated_candidate)
+    if(TRIGGER_WRONG_SEARCH)
+        # Query the GENERATED property to determine, whether the given file is a generated one.
+        # This is the line that should go into absolutify_source's implementation eventually.
+        get_source_file_property(_generated "${_generated_candidate}" TARGET_DIRECTORY "${target}" GENERATED)
+    else()
+        # To avoid querying the GENERATED property, hard-code the distinction between generated
+        # and non-generated files for this toy-example.
+        if(source STREQUAL "bar.h")
+            set(_generated TRUE)
+        else()
+            set(_generated FALSE)
+        endif()
+    endif()
+
+    set(${var} "${_generated}" PARENT_SCOPE)
+endfunction()
